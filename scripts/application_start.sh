299d7bcadaae17ce1a6cd0d4b7a901a5e3b5c7dc
arpitt29 <arpitchoudharymp41@gmail.com>
2025-10-30 17:50:41 +0530

    all necessary files added

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,17 +10,22 @@ then
 fi
 
 # --- REPLACE THESE VALUES ---
-DB_HOST="<YOUR_RDS_ENDPOINT>"
-DB_USER="<YOUR_RDS_MASTER_USERNAME>"
-DB_PASS="<YOUR_RDS_MASTER_PASSWORD>"
 AWS_ACCOUNT_ID="<YOUR_12_DIGIT_AWS_ACCOUNT_ID>"
 AWS_REGION="<YOUR_AWS_REGION_e.g.,_ap-south-1>"
-IMAGE_REPO_NAME="bitcot-php-app" # The ECR repo name we will create
+IMAGE_REPO_NAME="bitcot-php-app"
+DB_HOST="<YOUR_RDS_ENDPOINT>"
+
+# --- SECURELY FETCH CREDENTIALS ---
+# This is the professional way to handle secrets.
+# It uses the EC2 instance's IAM Role to get the parameters.
+DB_USER=$(aws ssm get-parameter --name "/bitcot-app/db-user" --with-decryption --region $AWS_REGION --query "Parameter.Value" --output text)
+DB_PASS=$(aws ssm get-parameter --name "/bitcot-app/db-pass" --with-decryption --region $AWS_REGION --query "Parameter.Value" --output text)
 
 # --- DO NOT EDIT BELOW THIS LINE ---
 IMAGE_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest"
 
-# Login to ECR and run the new container
+# Login to ECR (works via IAM Role)
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
 docker pull $IMAGE_URI
 docker run -d -p 80:80 --name php-container -e DB_HOST=$DB_HOST -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_NAME=webappdb $IMAGE_URI
